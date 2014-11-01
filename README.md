d0cf024faa400707a078ddd423a8f64c59b2cb53
brindldc <brindldc@rose-hulman.edu>
2014-11-01 11:17:26 -0400

    Update code
    Cleaned up and shortened leap motion code
    Deleted original Commits
    uploaded contemporary issues memo

--- README.md
+++
@@ -1,5 +0,0 @@
-1415-THCM
-=========
-
-Terre Haute Children's Museum 14-15
-Race car track exhibit with LeapMotion controller.
--- Sample.cpp
+++ Sample.cpp
@@ -24,49 +24,25 @@ public:
 private:
 };
 
-const std::string fingerNames[] = { "Thumb", "Index", "Middle", "Ring", "Pinky" };
-const std::string boneNames[] = { "Metacarpal", "Proximal", "Middle", "Distal" };
-const std::string stateNames[] = { "STATE_INVALID", "STATE_START", "STATE_UPDATE", "STATE_END" };
-
-void SampleListener::onInit(const Controller& controller) {
-	std::cout << "Initialized" << std::endl;
-}
-
-void SampleListener::onConnect(const Controller& controller) {
-	std::cout << "Connected" << std::endl;
-	controller.enableGesture(Gesture::TYPE_CIRCLE);
-	controller.enableGesture(Gesture::TYPE_KEY_TAP);
-	controller.enableGesture(Gesture::TYPE_SCREEN_TAP);
-	controller.enableGesture(Gesture::TYPE_SWIPE);
-}
-
-void SampleListener::onDisconnect(const Controller& controller) {
-	// Note: not dispatched when running in a debugger.
-	std::cout << "Disconnected" << std::endl;
-}
-
-void SampleListener::onExit(const Controller& controller) {
-	std::cout << "Exited" << std::endl;
-}
 
 void SampleListener::onFrame(const Controller& controller) {
 	// Get the most recent frame and report some basic information
 	const Frame frame = controller.frame();
 	int extendedFingers = 0;
-	/*std::cout << "Frame id: " << frame.id()
-	<< ", timestamp: " << frame.timestamp()
-	<< ", hands: " << frame.hands().count()
-	<< ", fingers: " << frame.fingers().count()
-	<< ", tools: " << frame.tools().count()
-	<< ", gestures: " << frame.gestures().count() << std::endl;*/
 
+	// Check if frame has something in it
 	if (!frame.hands().isEmpty()){
+		// Create an array of each hand in the frame
 		HandList hands1 = frame.hands();
-		Hand hand1 = hands1[0];
+		Hand currentHand = hands1[0];
+		// Loop through each hand in the frame
 		for (int i = 0; i < hands1.count(); i++){
-			hand1 = hands1[i];
-			for (int f = 0; f < hand1.fingers().count(); f++){
-				Finger finger = hand1.fingers()[f];
+			// Set the hand currently being analyzed
+			currentHand = hands1[i];
+			// Loop through each finger on the current hand
+			for (int f = 0; f < currentHand.fingers().count(); f++){
+				Finger finger = currentHand.fingers()[f];
+				// if the finger is extended, increase the count
 				if (finger.isExtended()) extendedFingers++;
 			}
 		}
@@ -79,15 +55,18 @@ void SampleListener::onFrame(const Controller& controller) {
 
 	}
 
+	// Emulates a keystroke
 	ip.type = INPUT_KEYBOARD;
-	ip.ki.wScan = 0; // hardware scan code for key
+	ip.ki.wScan = 0; 
 	ip.ki.time = 0;
 	ip.ki.dwExtraInfo = 0;
 
 	if (extendedFingers > 2){
 		extendedFingers = 2;
 	}
-	ip.ki.wVk = extendedFingers+48; // virtual-key code for the 0-2 keys
+	if(extendedFingers < 9){
+	ip.ki.wVk = extendedFingers+48; // virtual-key code for the 0-9 keys
+	} else extendedFingers = 9;
 	ip.ki.dwFlags = 0; // 0 for key press
 	SendInput(1, &ip, sizeof(INPUT));
 
@@ -95,93 +74,8 @@ void SampleListener::onFrame(const Controller& controller) {
 	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
 	SendInput(1, &ip, sizeof(INPUT));
 
-
-	HandList hands = frame.hands();
-	for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
-		// Get the first hand
-		const Hand hand = *hl;
-		/*std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
-		std::cout << std::string(2, ' ') << handType << ", id: " << hand.id()
-		<< ", palm position: " << hand.palmPosition() << std::endl;*/
-		// Get the hand's normal vector and direction
-		const Vector normal = hand.palmNormal();
-		const Vector direction = hand.direction();
-
-		// Calculate the hand's pitch, roll, and yaw angles
-		/*std::cout << std::string(2, ' ') <<  "pitch: " << direction.pitch() * RAD_TO_DEG << " degrees, "
-		<< "roll: " << normal.roll() * RAD_TO_DEG << " degrees, "
-		<< "yaw: " << direction.yaw() * RAD_TO_DEG << " degrees" << std::endl;*/
-
-		// Get the Arm bone
-		Arm arm = hand.arm();
-		/*std::cout << std::string(2, ' ') <<  "Arm direction: " << arm.direction()
-		<< " wrist position: " << arm.wristPosition()
-		<< " elbow position: " << arm.elbowPosition() << std::endl;*/
-
-		// Get fingers
-		const FingerList fingers = hand.fingers();
-		for (FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl) {
-			const Finger finger = *fl;
-			/*std::cout << std::string(4, ' ') <<  fingerNames[finger.type()]
-			<< " finger, id: " << finger.id()
-			<< ", length: " << finger.length()
-			<< "mm, width: " << finger.width() << std::endl;*/
-
-			// Get finger bones
-			for (int b = 0; b < 4; ++b) {
-				Bone::Type boneType = static_cast<Bone::Type>(b);
-				Bone bone = finger.bone(boneType);
-				/*std::cout << std::string(6, ' ') <<  boneNames[boneType]
-				<< " bone, start: " << bone.prevJoint()
-				<< ", end: " << bone.nextJoint()
-				<< ", direction: " << bone.direction() << std::endl;*/
-			}
-		}
-	}
-
-	// Get tools
-	const ToolList tools = frame.tools();
-	for (ToolList::const_iterator tl = tools.begin(); tl != tools.end(); ++tl) {
-		const Tool tool = *tl;
-		/*std::cout << std::string(2, ' ') <<  "Tool, id: " << tool.id()
-		<< ", position: " << tool.tipPosition()
-		<< ", direction: " << tool.direction() << std::endl;*/
-	}
-
-
-	/*if (frame.hands().isEmpty()) {
-	std::cout << "Empty Frame" << std::endl;
-	}
-	else{
-	std::cout << "HAND FOUND!!!" << std::endl;
-	}*/
 }
 
-void SampleListener::onFocusGained(const Controller& controller) {
-	std::cout << "Focus Gained" << std::endl;
-}
-
-void SampleListener::onFocusLost(const Controller& controller) {
-	std::cout << "Focus Lost" << std::endl;
-}
-
-void SampleListener::onDeviceChange(const Controller& controller) {
-	std::cout << "Device Changed" << std::endl;
-	const DeviceList devices = controller.devices();
-
-	for (int i = 0; i < devices.count(); ++i) {
-		std::cout << "id: " << devices[i].toString() << std::endl;
-		std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << std::endl;
-	}
-}
-
-void SampleListener::onServiceConnect(const Controller& controller) {
-	std::cout << "Service Connected" << std::endl;
-}
-
-void SampleListener::onServiceDisconnect(const Controller& controller) {
-	std::cout << "Service Disconnected" << std::endl;
-}
 
 int main(int argc, char** argv) {
 	// Create a sample listener and controller
@@ -191,8 +85,8 @@ int main(int argc, char** argv) {
 	// Have the sample listener receive events from the controller
 	controller.addListener(listener);
 
-	//if (argc > 1 && strcmp(argv[1], "--bg") == 0)
-		controller.setPolicyFlags(Leap::Controller::POLICY_BACKGROUND_FRAMES);
+	// Set this program to run in the background
+	controller.setPolicyFlags(Leap::Controller::POLICY_BACKGROUND_FRAMES);
 
 	// Keep this process running until Enter is pressed
 	std::cout << "Press Enter to quit..." << std::endl;
@@ -203,5 +97,3 @@ int main(int argc, char** argv) {
 
 	return 0;
 }
-
-
