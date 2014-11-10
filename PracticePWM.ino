dde8f6fe8d1b7af4418d90b228854ca4957b6729
pitschm1 <pitschm1@rose-hulman.edu>
2014-11-10 10:29:26 -0500

    Latest Version of Main

--- PracticePWM.ino
+++ PracticePWM.ino
@@ -1,37 +1,89 @@
 
 
+
 #include <Servo.h>
 #include <LiquidCrystal.h>
-
-
+#include <iostream>
+#include <windows.h>
 
 Servo servo1;
 
 #define PWM_PIN 11    
-#define BUTTON_RIGHT 2
-
+#define BUTTON_RIGHT 32
+#define IR_PIN_1 34
+#define IR_PIN_2 36
+#define LED_IR_1_OUT 26
+#define LED_IR_2_OUT 28
+#define B1_SSD 27
+#define C1_SSD 29
+#define D1_SSD 35
+#define A1_SSD 37
+#define B2_SSD 43
+#define C2_SSD 45
+#define D2_SSD 47
+#define A2_SSD 49
+#define B3_SSD 43
+#define C3_SSD 45
+#define D3_SSD 47
+#define A3_SSD 49
+#define LEDTest 65
 #define FLAG_INTERRUPT_0 0x01
-
+#define onesDigit  1
+#define tensDigit  2
+#define hundredsDigit  3
 
 volatile int mainEventFlags = 0;
 int delayTime = 2000;
 int v2;
 int delayCount;
+int ir1Reading;
+int ir2Reading;
 int count;
+int SSD_counter;
+int digCount;
+int decCount;
+int aPin;
+int bPin;
+int cPin;
+int dPin;
+int i;
+int j;
+int k;
+int SSDNum = 1;
+int A_SSD;
+int B_SSD;
+int C_SSD;
+int D_SSD;
 
 
 void setup()  {
+	//Yes this setup is a bit disgusting, but there's no other way to do it
 	pinMode(1, OUTPUT);
+	pinMode(IR_PIN_1, INPUT);
+	pinMode(IR_PIN_2, INPUT);
+	pinMode(LED_IR_1_OUT, OUTPUT);
+	pinMode(LED_IR_2_OUT, OUTPUT);
+	pinMode(A1_SSD, OUTPUT);
+	pinMode(B1_SSD, OUTPUT);
+	pinMode(C1_SSD, OUTPUT);
+	pinMode(D1_SSD, OUTPUT);
+	pinMode(A2_SSD, OUTPUT);
+	pinMode(B2_SSD, OUTPUT);
+	pinMode(C2_SSD, OUTPUT);
+	pinMode(D2_SSD, OUTPUT);
+	pinMode(LEDTest, OUTPUT);
+	pinMode(BUTTON_RIGHT, INPUT_PULLUP);
 	Serial.begin(9600);
 	servo1.attach(A7);
+	digitalWrite(LEDTest, HIGH);
 
-
-	attachInterrupt(0, int0_isr, FALLING);
+	//attachInterrupt(1, int0_isr, FALLING);
 
 }
 
 void loop() {
-	pinMode(BUTTON_RIGHT, INPUT_PULLUP);
+
+	// Control servo angle based on serial input
 	if (Serial.available() > 0){
 		char letter = Serial.read();
 		if (letter == '1'){
@@ -43,62 +95,161 @@ void loop() {
 		else if (letter == '0'){
 			count = 0;
 		}
-		else if (letter == '3'){
-			servo1.write(180);
-			delay(15);
-		}
-		else if (letter == '4'){
-			servo1.write(0);
-			delay(15);
-		}
-		else if (letter == '5'){
-			servo1.detach();
-			
-		}
 	}
 
-
-
+	// Set the PWM value to the track 0-255
 	if (count == 1){
-		digitalWrite(pinChoice, LOW);
 		v2 = 125;
-		
+
 	}
 	else if (count == 2){
-		digitalWrite(pinChoice, LOW);
 		v2 = 255;
-		
+
 	}
 	else{
-		digitalWrite(pinChoice, LOW);
 		v2 = 0;
-		
+
 
 	}
 
-	if (mainEventFlags & FLAG_INTERRUPT_0) {
-		delay(20);
-		mainEventFlags &= ~FLAG_INTERRUPT_0;
-		if (!digitalRead(BUTTON_RIGHT)) {
-			servo1.write(0);
-			delay(1000);
-			servo1.write(120);
-			delay(1000);
-			servo1.write(0);
-			delay(1000);
-			servo1.write(120);
-			delay(1000);
-		}
+	//  If the button is pressed wave the flag back and forth
+	//if (mainEventFlags & FLAG_INTERRUPT_0) {
+	//delay(20);
+	//mainEventFlags &= ~FLAG_INTERRUPT_0;
+	if (digitalRead(BUTTON_RIGHT) == 1) {
+		digitalWrite(LEDTest, LOW);
+		servo1.write(0);
+		delay(300);
+		servo1.write(120);
+		delay(300);
+		servo1.write(0);
+		delay(300);
+		servo1.write(120);
+		delay(300);
+		servo1.write(0);
+		delay(300);
+		servo1.write(120);
+		delay(300);
+		servo1.write(0);
+		delay(300);
+		servo1.write(120);
+		delay(300);
+	}
 
+	 //Dis supposed ta be countin'
+	for (k = 0; k < 10; k++){
+		writeDigits(hundredsDigit, k);
+		for (j = 0; j < 10; j++){
+			writeDigits(tensDigit, j);
+			for (i = 0; i < 10; i++){
+				writeDigits(onesDigit, i);
+				delay(1000);
+			}
+		}
 	}
 	
+	// Read motion sensors and set each section of LEDs
+	if (digitalRead(IR_PIN_1) == LOW){
+		digitalWrite(LED_IR_1_OUT, HIGH);
+		digitalWrite(LED_IR_2_OUT, LOW);
+	}
+
+	if (digitalRead(IR_PIN_2) == LOW){
+		digitalWrite(LED_IR_2_OUT, HIGH);
+		digitalWrite(LED_IR_1_OUT, LOW);
+	}
 
 	analogWrite(PWM_PIN, v2);
-	digitalWrite(pinChoice, HIGH);
 
-	
+
 }
 
-void int0_isr(){
-	mainEventFlags |= FLAG_INTERRUPT_0;
+//void int0_isr(){
+	//mainEventFlags |= FLAG_INTERRUPT_0;
+//}
+
+void writeDigits(char SSDNum, char number){
+
+	//Decide which Seven Segment Display to write to
+	// 1 = ones digit   2 = tens digit  3 = hundreds digit
+	if (SSDNum == 1){
+		A_SSD = A1_SSD;
+		B_SSD = B1_SSD;
+		C_SSD = C1_SSD;
+		D_SSD = D1_SSD;
+	}
+	else if (SSDNum == 2){
+		A_SSD = A2_SSD;
+		B_SSD = B2_SSD;
+		C_SSD = C2_SSD;
+		D_SSD = D2_SSD;
+	}
+	else if (SSDNum == 3){
+		A_SSD = A3_SSD;
+		B_SSD = B3_SSD;
+		C_SSD = C3_SSD;
+		D_SSD = D3_SSD;
+	}
+
+	// Write the numbers 0-9
+	if (number == 1){
+		digitalWrite(A_SSD, HIGH);
+		digitalWrite(B_SSD, LOW);
+		digitalWrite(C_SSD, LOW);
+		digitalWrite(D_SSD, LOW);
+	}
+	else if (number == 2){
+		digitalWrite(A_SSD, LOW);
+		digitalWrite(B_SSD, HIGH);
+		digitalWrite(C_SSD, LOW);
+		digitalWrite(D_SSD, LOW);
+	}
+	else if (number == 3){
+		digitalWrite(A_SSD, HIGH);
+		digitalWrite(B_SSD, HIGH);
+		digitalWrite(C_SSD, LOW);
+		digitalWrite(D_SSD, LOW);
+	}
+	else if (number == 4){
+		digitalWrite(A_SSD, LOW);
+		digitalWrite(B_SSD, LOW);
+		digitalWrite(C_SSD, HIGH);
+		digitalWrite(D_SSD, LOW);
+	}
+	else if (number == 5){
+		digitalWrite(A_SSD, HIGH);
+		digitalWrite(B_SSD, LOW);
+		digitalWrite(C_SSD, HIGH);
+		digitalWrite(D_SSD, LOW);
+	}
+	else if (number == 6){
+		digitalWrite(A_SSD, LOW);
+		digitalWrite(B_SSD, HIGH);
+		digitalWrite(C_SSD, HIGH);
+		digitalWrite(D_SSD, LOW);
+	}
+	else if (number == 7){
+		digitalWrite(A_SSD, HIGH);
+		digitalWrite(B_SSD, HIGH);
+		digitalWrite(C_SSD, HIGH);
+		digitalWrite(D_SSD, LOW);
+	}
+	else if (number == 8){
+		digitalWrite(A_SSD, LOW);
+		digitalWrite(B_SSD, LOW);
+		digitalWrite(C_SSD, LOW);
+		digitalWrite(D_SSD, HIGH);
+	}
+	else if (number == 9){
+		digitalWrite(A_SSD, HIGH);
+		digitalWrite(B_SSD, LOW);
+		digitalWrite(C_SSD, LOW);
+		digitalWrite(D_SSD, HIGH);
+	}
+	else if (number == 0){
+		digitalWrite(A_SSD, LOW);
+		digitalWrite(B_SSD, LOW);
+		digitalWrite(C_SSD, LOW);
+		digitalWrite(D_SSD, LOW);
+	}
 }
